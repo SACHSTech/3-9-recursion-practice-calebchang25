c7c8e174f4831b1919ea632a6417720eb2af0426
calebchang25 <caleb.chang25@ycdsbk12.ca>
2024-11-27 00:43:36 -0500

    Worked out a framework solution that includes one for loop

--- src/recursion/SubsetsOfASet.java
+++ src/recursion/SubsetsOfASet.java
@@ -4,13 +4,13 @@ public class SubsetsOfASet {
     public static void generateSubsets(String s){
         if(s.equals("") == false){
             System.out.print(s + ", ");
-            generateSubsets(s.substring(0, s.length()-1));
-            generateSubsets(s.substring(1, s.length()));
-            // fix by somehow removing the middle one
+            for(int x = 1; x < s.length(); x++){
+                System.out.print(s.substring(0, s.length()-x) + ", ");
+            }
+            generateSubsets(s.substring(1));
         }
-
     }
     public static void main(String[] args) {
-        generateSubsets("abcde");
+        generateSubsets("abcd");
     }
 }
