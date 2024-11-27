75679655665cbf6304cfc77722ff0ba9ccb9b4d5
calebchang25 <caleb.chang25@ycdsbk12.ca>
2024-11-26 23:45:13 -0500

    Edit to formating for final question

--- src/recursion/SubsetsOfASet.java
+++ src/recursion/SubsetsOfASet.java
@@ -3,7 +3,7 @@ package recursion;
 public class SubsetsOfASet {
     public static void generateSubsets(String s){
         if(s.equals("") == false){
-            System.out.println(s);
+            System.out.print(s + ", ");
             generateSubsets(s.substring(0, s.length()-1));
             generateSubsets(s.substring(1, s.length()));
             // fix by somehow removing the middle one
